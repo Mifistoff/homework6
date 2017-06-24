c8bff4d6e0d6c3e0e528ea3c1fcbc5205b3cb119
Michael Tarasyuk <michael.tarasyuk91@gmail.com>
2017-06-25 00:11:41 +0300

    Some chenges for routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,24 +1,27 @@
 Rails.application.routes.draw do
+  devise_scope :user do
+    root to: 'devise/sessions#new'
+  end
+
   devise_for :admins
   devise_for :users
 
- namespace :admin do
-   resources :admins
-   resources :tasks, only: %i[index show update]
-   resources :comments, only: %i[new create]
-   resources :users
- end
+  resource :dashboard, only: %i[show], controller: :dashboard
+  resources :comments, only: %i[new create]
+  resources :tasks, only: %i[index new show create]
+
+  namespace :admin do
+    root 'dashboard#show'
+    resources :admins
+    resources :comments, only: %i[new create]
+    resources :tasks, only: %i[index show update]
+    resources :users
+  end
 
   namespace :api do
     namespace :v1 do
-      resources :tasks, only: %i[index new create show]
       resources :comments, only: %i[index new create show]
+      resources :tasks, only: %i[index new create show]
     end
   end
-
-  resources :tasks, only: %i[index new show create]
-  resources :comments, only: %i[new create]
-
-
-  root 'tasks#index'
 end
