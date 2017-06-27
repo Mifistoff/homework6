3a85caf80fae03b5065cb22594047a54e27c615c
Michael Tarasyuk <michael.tarasyuk91@gmail.com>
2017-06-27 15:38:49 +0300

    Deleted not used line

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,4 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   devise_group :visitor, contains: [:user, :admin]
-  #before_action :authenticate_visitor!
 end
