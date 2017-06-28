0a27dda95b2f133adc52145f2464b1fd511f5290
Michael Tarasyuk <michael.tarasyuk91@gmail.com>
2017-06-28 16:10:25 +0300

    Seeds chenged

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,17 +6,15 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-Admin.create (1..3).inject([]) { |command, n| command << {
-  email: "admin#{n}@admin", password: 'qwerty', password_confirmation: 'qwerty'
-}; command }
+Admin.create(email: 'admin@admin', password: 'NotAdmin', password_confirmation: 'NotAdmin')
 
-User.create (1..5).inject([]) { |command, n| command << {
+User.create (1..4).inject([]) { |command, n| command << {
   email: "user#{n}@user", password: 'qwerty', password_confirmation: 'qwerty'
 }; command }
 
 Task.create!([
   { title: 'Dog goes', body: 'woof', user_id: 1 },
-  { title: 'Cat goes', body: 'meow', user_id: 5 },
+  { title: 'Cat goes', body: 'meow', user_id: 2 },
   { title: 'Bird goes', body: 'tweet', user_id: 4 },
   { title: 'Mouse goes', body: 'squeek', user_id: 3 }
 ])
